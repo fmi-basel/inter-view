9ccff8d88c3ecf30a5d03533433be6c8deca0132
rfortiz <raphael.ortiz@fmi.ch>
2019-03-28 15:08:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Inter-View
+
+Interactive visualization of segmentation data
+
+## Installation
+TODO
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+contrib = [
+    'Raphael Ortiz',
+]
+
+# setup.
+setup(
+    name='inter_view',
+    version='0.1',
+    description='Utils to interactively visualize pre-computed dataframe with bokeh',
+    author=', '.join(contrib),
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    install_requires=[
+        'numpy>=1.15.4', 
+        'matplotlib>=3.0.2',
+        'bokeh>=1.0.4',
+        'pandas>=0.23.4',
+        'seaborn>=0.9.0',
+    ],
+    zip_safe=False)
+
